0ef3b5b44afd14ad22088b22abb7f88e71ab4b9c
billy8407 <billy8407@gmail.com>
2022-11-16 21:04:59 +0800

    Change name from links to urls

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import asyncio
 
+from utiltiy.content_handler import content_task
 from utiltiy.file_handler import File
-from utiltiy.links_handler import links_task
+from utiltiy.urls_handler import urls_task
 
 
 def download_data():
     # All pages's urls
-    links = asyncio.run(
-        links_task()
+    urls = asyncio.run(
+        urls_task()
     )
     
     # Keep urls in a file.
-    File.write_json('links.json', links)
+    File.write_json('urls.json', urls)
     
+    r = asyncio.run(
+        content_task(urls)
+    )
+
 
 if __name__ == '__main__':
     download_data()
--- utiltiy/urls_handler.py
+++ utiltiy/urls_handler.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 import requests
 
 
-async def get_links(loop, i):
-    page_links = []
+async def get_urls(loop, i):
+    page_urls = []
 
     print("Get link loop %d" %i)
 
-    # Get All page links.
+    # Get All page urls.
     url = "https://www.whitehouse.gov/briefing-room/speeches-remarks/page/%d/" %i
    
     
@@ -21,29 +21,29 @@ async def get_links(loop, i):
     if r.status_code != 200:
         print("loop %d failed in status code : %d", i, r.status_code)
     
-    # Fetch the page links.
+    # Fetch the page urls.
     soup = BeautifulSoup(r.text, "html.parser")
     items = soup.find_all(class_='news-item__title')
-    page_links: list[str] = [_['href'] for _ in items]
+    page_urls: list[str] = [_['href'] for _ in items]
 
-    return page_links
+    return page_urls
 
 
-async def links_task():
+async def urls_task():
     '''
-    Use asyncio for links crawling.
+    Use asyncio for urls crawling.
     '''
 
     loop = asyncio.get_event_loop()
 
-    # Get links from page 1 to 122.
-    search_work = [get_links(loop, i) for i in range(1, 123)]
+    # Get urls from page 1 to 122.
+    search_work = [get_urls(loop, i) for i in range(1, 123)]
 
     results: list[list[str]] = await asyncio.gather(*search_work) 
 
     # Use Numpy's flatten method in the list.
-    links: list[str] = [item for sublist in results for item in sublist]
+    urls: list[str] = [item for sublist in results for item in sublist]
 
-    print('Get page links finished!')
+    print('Get page urls finished!')
 
-    return links
+    return urls
